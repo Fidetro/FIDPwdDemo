1ab440723c2a9d511bdbacf486208f450438bad6
fidetro <zykzzzz@hotmail.com>
2017-02-16 22:16:18 +0800

    Initial Commit

---
+++ FIDPwdDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FIDPwdDemo
+//
+//  Created by Fidetro on 2017/2/16.
+//  Copyright © 2017年 Fidetro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FIDPwdDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FIDPwdDemo
+//
+//  Created by Fidetro on 2017/2/16.
+//  Copyright © 2017年 Fidetro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
